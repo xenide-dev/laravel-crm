4d72eed262095735cb1dff4461874b23061bb16e
Anthony Gacis <anthony.gacis@yahoo.com>
2020-10-24 19:17:32 +0800

    Added configs for dummy accts

--- config/_general.php
+++ config/_general.php
@@ -3,5 +3,9 @@
 return [
 
     // load only google analytics if it is in non-local environment
-    "load_ga" => (env("APP_ENV") == "local") ? false : true
+    "load_ga" => (env("APP_ENV") == "local") ? false : true,
+
+    // for dummy accts
+    "activate_dummy"    => env("ACTIVATE_DUMMY", false),
+    "no_of_dummy_accts" => env("NO_OF_DUMMY_ACCTS", 5)
 ];
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -88,63 +88,44 @@ class UserSeeder extends Seeder
         }
 
         // =============================== For other test super-admin ===============================
-        // generate 9 random super admin
-        for($i = 0; $i < 10; $i++){
-            $tempEmail = $faker->freeEmail;
-            $firstName = $faker->firstNameMale;
-            $middleName = $faker->lastName;
-            $lastName = $faker->lastName;
-            $userID = DB::table('users')->insertGetId([
-                'fname' => $firstName,
-                'mname' => $middleName,
-                'lname' => $lastName,
-                'full_name' => ucwords(sprintf("%s %s %s", $firstName, $middleName, $lastName)),
-                'id_number' => $faker->randomNumber(6),
-                'phone_number' => $faker->randomNumber(5),
-                'email' => $tempEmail,
-                'password' => Hash::make($tempEmail),
-                'isPassChanged' => 1,
-                'user_type' => "super-admin",
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'api_token' => Hash::make(now()),
-            ]);
+        if(config("_general.activate_dummy")) {
+            for($i = 0; $i < config("_general.no_of_dummy_accts"); $i++){
+                $tempEmail = $faker->freeEmail;
+                $firstName = $faker->firstNameMale;
+                $middleName = $faker->lastName;
+                $lastName = $faker->lastName;
+                $userID = DB::table('users')->insertGetId([
+                    'fname' => $firstName,
+                    'mname' => $middleName,
+                    'lname' => $lastName,
+                    'full_name' => ucwords(sprintf("%s %s %s", $firstName, $middleName, $lastName)),
+                    'id_number' => $faker->randomNumber(6),
+                    'phone_number' => $faker->randomNumber(5),
+                    'email' => $tempEmail,
+                    'password' => Hash::make($tempEmail),
+                    'isPassChanged' => 1,
+                    'user_type' => "super-admin",
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'api_token' => Hash::make(now()),
+                ]);
 
-            // populate the user's permission
-            // by default all
-            // get the config in _privileges.php
-            $configs = config("_privileges.urls");
-            foreach ($configs as $config){
-                foreach ($config["access"] as $access){
-                    DB::table('user_permissions')->insert([
-                        'user_id' => $userID,
-                        "name" => $config["name"],
-                        "slug" => Str::slug($access . " " . $config["name"]),
-                        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                    ]);
+                // populate the user's permission
+                // by default all
+                // get the config in _privileges.php
+                $configs = config("_privileges.urls");
+                foreach ($configs as $config){
+                    foreach ($config["access"] as $access){
+                        DB::table('user_permissions')->insert([
+                            'user_id' => $userID,
+                            "name" => $config["name"],
+                            "slug" => Str::slug($access . " " . $config["name"]),
+                            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                        ]);
+                    }
                 }
             }
         }
-
-
-        // for 20 random acct with 'user' account type
-//        for($i = 0; $i < 20; $i++){
-//            $faker = Faker\Factory::create();
-//            $firstName = $faker->firstNameMale;
-//            DB::table('users')->insert([
-//                'fname' => $firstName,
-//                'mname' => $faker->lastName,
-//                'lname' => $faker->lastName,
-//                'id_number' => $faker->randomNumber(6),
-//                'phone_number' => $faker->randomNumber(5),
-//                'email' => $faker->freeEmail,
-//                'password' => Hash::make($firstName),
-//                'user_type' => "user",
-//                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-//                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-//                'api_token' => Hash::make(now()),
-//            ]);
-//        }
     }
 }
